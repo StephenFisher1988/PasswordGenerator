2ee059810dc8675b366570d9d9a64395137f4fcb
Stephen Fisher <104858610+StephenFisher1988@users.noreply.github.com>
2022-11-06 17:51:32 -0600

    Update generator.py

--- generator.py
+++ generator.py
@@ -84,12 +84,77 @@ def spell_check(string1, string2, usebool):
     root.wait_window(top)
     
 
+# Function for deleting the password to the local file
+def delete_entry():
+    # Accountfor case
+    if option_var != "":
+        user_input = option_var.get()
+        user_input = user_input.upper()
+        option_var.set(site_option[0])
+    elif option_var != "" and for_entry.get() != "":
+        user_input = for_entry.get()
+        user_input = user_input.upper()
+        option_var.set(site_option[0])
+    else:
+        user_input = for_entry.get()
+        user_input = user_input.upper()
+    if len(entry.get()) != 0:
+        found = False # Boolean for determining if a line exists
+        # Read through file line by line
+        with open(os.path.join(sys.path[0], "test.txt"), "r") as pf:
+            # Stores the line data as strings in the list "Line"
+            line = pf.readlines()
+            word = []
+            # Separate the string stored in the list "line" into "word" list by the ":" delimiter
+            for item in line:
+                word.append(item.split(":"))
+            # Flatten the "word" list
+            flatlist=[]
+            for sublist in word:
+                for element in sublist:
+                    flatlist.append(element)
+            # Determine if the user has a password saved already then update flatlist
+            itvar = 0
+            for i in flatlist:
+                # Spell Check
+                if Levenshtein.ratio(user_input, flatlist[itvar])<1 and Levenshtein.ratio(user_input, flatlist[itvar])>0.4:
+                    spell_check(user_input, flatlist[itvar], user_choice)
+                    if user_choice == True:
+                        user_input = flatlist[itvar]
+                if i == user_input:
+                    del flatlist[itvar + 1]
+                    del flatlist[itvar]
+                    found = True
+                itvar = itvar + 1
+        # Remove the whitespace as it causes a bug
+        flatlist = [w.strip(' ') for w in flatlist]
+        if found == True:
+            # Put it all together into one string then overwrite the file
+            file1 = open(os.path.join(sys.path[0], "test.txt"), "w")
+            flatlist = ''.join(flatlist)
+            file1.write(flatlist)
+    # Update text field
+    file1 = open(os.path.join(sys.path[0], "test.txt"), "r")
+    text.delete("1.0","end")
+    for i in file1:
+        text.insert(END, i)
+    file1.close()
+
 
 # Function for saving the password to the local file
 def update_file():
     # Accountfor case
-    user_input = for_entry.get()
-    user_input = user_input.upper()
+    if option_var != "":
+        user_input = option_var.get()
+        user_input = user_input.upper()
+        option_var.set(site_option[0])
+    elif option_var != "" and for_entry.get() != "":
+        user_input = for_entry.get()
+        user_input = user_input.upper()
+        option_var.set(site_option[0])
+    else:
+        user_input = for_entry.get()
+        user_input = user_input.upper()
     if len(entry.get()) != 0:
         found = False # Boolean for determining if a line exists
         # Read through file line by line
@@ -145,23 +210,38 @@ def update_file():
 w = ""
 user_input = ""
 user_choice = False
+site_option = [
+"",
+"Amazon",
+"Facebook",
+"Google",
+"Hulu",
+"LinkedIn",
+"Netflix",
+"StackOverflow",
+"Yahoo"
+]
 
 # Create GUI window
 root = Tk()
-root.geometry("600x500")
+root.geometry("470x380")
 var = IntVar()
 var1 = IntVar()
 ft = tkFont.Font(family='Times',size=10)
 st = tkFont.Font(family='Times',size=12)
+option_var = StringVar(root)
+option_var.set(site_option[0]) # Default option
  
 # Title of your GUI window
-root.title("Password Generator v0.1")
+root.title("Password Generator v0.2")
 
 # Create a label for what the password is for
 f_label = Label(root, text="Site", relief="flat", font=ft, justify="left")
 f_label.place(x=20,y=20,width=70,height=25)
+option_menu = OptionMenu(root, option_var, *site_option)
+option_menu.place(x=100,y=20,width=70,height=25)
 for_entry = Entry(root)
-for_entry.place(x=100,y=20,width=70,height=25)
+for_entry.place(x=190,y=20,width=70,height=25)
 
 # Create label and entry to show the password generated
 Random_password = Label(root, text="Password", relief="flat", font=ft, justify="left")
@@ -181,6 +261,8 @@ generate_button = Button(root, text="Generate", command=generate)
 generate_button.place(x=280,y=60,width=70,height=25)
 save_button = Button(root, text="Save", command=update_file)
 save_button.place(x=370, y=60, width=70, height=25)
+delete_button = Button(root, text="Delete", command=delete_entry)
+delete_button.place(x=280, y=20, width=70, height=25)
  
 # Radio Buttons for deciding the strength of the password. Default is medium
 radio_low = Radiobutton(root, text="Low", variable=var, value=1)
@@ -204,7 +286,7 @@ combo.place(x=100,y=100,width=70,height=25)
 # Create a text box that shows the currently saved passwords
 # Add a Scrollbar(horizontal)
 v=Scrollbar(root, orient='vertical')
-v.place(x=10,y=290)
+v.place(x=10,y=1700)
 
 
 # Add a text widget
@@ -212,7 +294,7 @@ text=Text(root, font=("Arial, 12"), yscrollcommand=v.set)
 
 # Create a label for the text box
 s_label= Label(root, text="Saved Passwords", relief="flat", font=st, justify="left")
-s_label.place(x=10,y=260,width=150,height=25)
+s_label.place(x=10,y=140,width=150,height=25)
 
 
 # Add some text in the text widget
@@ -222,7 +304,7 @@ for i in file1:
 
 # Attach the scrollbar with the text widget
 v.config(command=text.yview)
-text.place(x=10,y=290,width=570,height=195)
+text.place(x=10,y=170,width=440,height=195)
  
 # start the GUI
 root.mainloop()
